5c4d36aa27d52b7ff4eeadd7d0dabaf07c3ba3d3
Gilberto Mautner <gilberto.mautner@locaweb.com.br>
2026-01-08 11:02:23 -0500

    Use HTTPS module source for GitHub Actions compatibility

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "cluster" {
-  source = "git@github.com:gmautner/locaweb-platform.git//modules/cloudstack-k3s"
+  source = "github.com/gmautner/locaweb-platform//modules/cloudstack-k3s"
 
   cluster_prefix        = var.cluster_prefix
   cloudstack_api_key    = var.cloudstack_api_key
